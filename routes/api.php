e500ed8c17f491574bc3c9cc7dac34739adc7aa1
MarioCruz20 <blastedcore2025@gmail.com>
2026-03-31 21:47:25 -0600

    Definicion de ruta de la API para tabla Product

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductionLineController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,7 +20,7 @@ Route::get('/productionline/obtener/{id}', function($id) {
     return $controller->obtenerPorId($id);
     });
 
-//Ejemplo de como llamar al controlador durectamente
+//Ejemplo de como llamar al controlador directamente
 Route::delete('/productionline/eliminar/{id}', [ProductionLineController::class, 'eliminarPorId']);
 
 //Esta ruta le pasa el request al controlador
@@ -28,6 +29,32 @@ Route::put('/productionline/actualizar', [ProductionLineController::class, 'actu
 
 //Route para crear
 Route::post('/productionline/crear', [ProductionLineController::class, 'crear']);
+
+
+//Definicion del endpoint:
+//GET: normalmente se usa para datos que son solicitados
+Route::get('/product/obtener/todos', function() {
+
+    $controller = new ProductController();
+    return $controller->obtenerTodos();
+});
+
+//Aquí se crea una ruta con parametros dinamicos
+//es decir {id}
+Route::get('/product/obtener/{id}', function($id) {
+    $controller = new ProductController();
+    return $controller->obtenerPorId($id);
+    });
+
+//Ejemplo de como llamar al controlador directamente
+Route::delete('/product/eliminar/{id}', [ProductController::class, 'eliminarPorId']);
+
+//Esta ruta le pasa el request al controlador
+//y el controlador lo recibe
+Route::put('/product/actualizar', [ProductController::class, 'actualizarPorId']);
+
+//Route para crear
+Route::post('/product/crear', [ProductController::class, 'crear']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
